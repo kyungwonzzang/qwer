e9f4c55da7621c01b0af87f56e7eddc994ac24d2
kyungwonzzang <83160767+kyungwonzzang@users.noreply.github.com>
2021-06-15 15:39:03 +0900

    pythonanywhere 사이트 배포 정보 수정

--- app.py
+++ app.py
@@ -76,13 +76,5 @@ def login():
             session['user'] = ret[3] # 로그인 처리
         return ck_idpw(ret)
 
-if __name__ == '__main__':
-    app.run()
-
-# 웹브라우저에 http://127.0.0.1:5000/naver
-# 위와같이 접속 하면 안녕 나는 네이버야~
-# 라는 글자가 나타나게 하기
-
-# id == a
-# pw == 123
-# 로그인 했을때 맞으면 성공 , 틀리면 실패 만들어보기
+# if __name__ == '__main__':
+#     app.run()
